d8f475739df28fd87d06e633b79ab4f55e9a2c8b
Nathachai Thongniran <inid3a@gmail.com>
2020-02-27 19:01:11 +0700

    Fix TypeError: 'module' object is not callable

---
+++ leoprofanity/__init__.py
@@ -0,0 +1,11 @@
+"""leoprofanity
+    >>> from leoprofanity import LeoProfanity
+    >>> fil = LeoProfanity()
+    >>> fil.clean("I have boob, etc.") # "I have ****, etc."
+"""
+
+# import leoprofanity
+# leoprofanity.__version__
+__version__ = "0.0.1"
+
+from leoprofanity.LeoProfanity import LeoProfanity  # noqa: F401
